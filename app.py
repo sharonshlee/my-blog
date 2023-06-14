f02582db169013e2451125f84b5caa01c85b8f35
Sharon Lee <sharonshlee@icloud.com>
2023-06-14 10:02:23 -0500

    added index route, reading blog posts from json file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def index():
+    with open('static/posts.json', 'r') as file:
+        blog_posts = json.load(file)
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == "__main__":
