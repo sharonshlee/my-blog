f8ce9904115721ff58037af6c943e79557b3c81f
Sharon Lee <sharonshlee@icloud.com>
2023-06-14 14:23:01 -0500

    added delete route

--- app.py
+++ app.py
@@ -23,7 +23,13 @@ def add_post(blog_posts):
         'title': request.form.get('title', ''),
         'content': request.form.get('content', '')
     })
-    return blog_posts
+
+
+def delete_post(blog_posts, post_id):
+    for post in blog_posts:
+        if post['id'] == post_id:
+            blog_posts.remove(post)
+    return None
 
 
 @app.route('/')
@@ -39,6 +45,18 @@ def add():
         write_post(blog_posts)
         return redirect(url_for('index'))
     return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    """
+    Remove the blog post with given id from the list
+    Redirect back to the home page
+    """
+    blog_posts = read_posts_file()
+    delete_post(blog_posts, post_id)
+    write_post(blog_posts)
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
